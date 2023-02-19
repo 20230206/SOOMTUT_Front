e6a6e2c5e9f7a93d3b97057ad1c412f0c6ad2780
lalyns <kelwho@naver.com>
2023-02-19 23:02:28 +0900

    #1 [Test] 빌드 성공 테스트

--- src/components/SoomtutNavbar.js
+++ src/components/SoomtutNavbar.js
@@ -8,120 +8,107 @@ import logo from '../assets/images/logo.png'
 
 
 function SoomtutNavbar() {
-    axios.defaults.withCredentials = true;
+  axios.defaults.withCredentials = true;
 
-    const [signin, setSignin] = useState(false);
-    const [token, setToken] = useState(null);
-    const [name, setName] = useState("...");
-    const [userData, setUserData] = useState(null);
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [token, setToken] = useState(null);
+  const [name, setName] = useState("...");
 
-    const IsSignnedIn = useCallback (async () => {
-        var config = {
-            method: 'get',
-            maxBodyLength: Infinity,
-            url: 'http://localhost:8080/auth/validtoken',
-            headers: { }
-          };
-          
-        try {
-            const response = await axios(config);
-            setToken(response.headers.get("Authorization"));
-            setSignin(response.data);
-        }
-        catch(error) {
-            console.log(error)
-            setToken(null);
-        }
-
-        if (!token) {
-            setUserData(null);
-            return;
-        }
-    }, []);
-
-    const signout = async () => {
-        // 서버에 로그아웃 요청 보내고 refresh cookie를 삭제해준다
-        var config = {
-            method: 'post',
-            maxBodyLength: Infinity,
-            url: 'http://localhost:8080/auth/signout'
-        };
-        
-        try {
-            const response = await axios(config)
-            setSignin(response.data);
-        } catch(error) {
-            console.log(error);
-        }
-    
-
-        // 화면을 새로고침 해준다
-        window.location.reload();
+  const checkTokenValidity = useCallback(async () => {
+    const config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:8080/auth/validtoken',
+      headers: {}
+    };
+    try {
+      const response = await axios(config);
+      setToken(response.headers.get("Authorization"));
+      setIsSignedIn(response.data);
+    } catch (error) {
+      console.log(error);
+      setToken(null);
     }
-    
-    useEffect(() => {
-        IsSignnedIn();
-    }, [IsSignnedIn])
+  }, []);
 
-    useEffect(() => {
-        const fetchUserData = async() => {
-            var config = {
-                method: 'get',
-                maxBodyLength: Infinity,
-                url: 'http://localhost:8080/getmyinfo',
-                headers: { 
-                  'Authorization': token
-                }
-              };
-            try {
-                const response = await axios(config)
-                setUserData(response.data)
-            
-            } catch(error) {
-                console.log(error);
+  const handleSignout = async () => {
+    // Send a logout request to the server and delete the refresh cookie
+    const config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:8080/auth/signout'
+    };
+
+    try {
+      const response = await axios(config);
+      setIsSignedIn(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // Refresh the page
+    window.location.reload();
+  }
+
+  useEffect(() => {
+    checkTokenValidity();
+  }, [checkTokenValidity])
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:8080/getmyinfo',
+        headers: {
+          'Authorization': token
+        }
+      };
+      try {
+        const response = await axios(config);
+        setName(response.data.nickname);
+
+      } catch (error) {
+        console.log(error);
+      }
+
+    }
+
+    if (isSignedIn === true) {
+      fetchUserData();
+    }
+
+  }, [isSignedIn, token])
+
+  return (
+    <div className={styles.wrapper}>
+      <Navbar bg="white" variant="white">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={logo}
+              width="240"
+              className="d-inline-block align-top"
+            />{' '}
+          </Navbar.Brand>
+          <div className={styles.navmenu}>
+            {isSignedIn &&
+              <Navbar.Text className={styles.navmenuitem}>{name}님 안녕하세요</Navbar.Text>
             }
-            
-        }
-                
-        if (signin === true) {
-            fetchUserData();
-        }
-            
-    }, [signin, token])
-
-    useEffect(() => {
-        setName(userData.nickname)
-    }, [userData])
-    
-    return (
-        <div className={styles.wrapper}>
-         <Navbar bg="white" variant="white">
-             <Container>
-             <Navbar.Brand href="#home">
-                 <img
-                 alt=""
-                 src={logo}
-                 width="240"
-                 className="d-inline-block align-top"
-                 />{' '}
-             </Navbar.Brand>
-                <div className={styles.navmenu}>
-                    {signin &&
-                        <Navbar.Text className={styles.navmenuitem}>{name}님 안녕하세요</Navbar.Text> 
-                    }
-                    {signin ? 
-                        <Nav.Link className={styles.navmenuitem} onClick={() => signout()}> 로그아웃 </Nav.Link> : 
-                        <Nav.Link className={styles.navmenuitem} href="/signin"> 로그인 </Nav.Link> 
-                    }
-                    {signin ? 
-                        <Nav.Link className={styles.navmenuitem} href="/mypage"> 마이페이지 </Nav.Link>  : 
-                        <Nav.Link className={styles.navmenuitem} href="/signup"> 회원가입 </Nav.Link> 
-                    }
-                </div>
-             </Container>
-         </Navbar>
-        </div>
-    );
+            {isSignedIn ?
+              <Nav.Link className={styles.navmenuitem} onClick={handleSignout}> 로그아웃 </Nav.Link> :
+              <Nav.Link className={styles.navmenuitem} href="/signin"> 로그인 </Nav.Link>
+            }
+            {isSignedIn ?
+              <Nav.Link className={styles.navmenuitem} href="/mypage"> 마이페이지 </Nav.Link> :
+              <Nav.Link className={styles.navmenuitem} href="/signup"> 회원가입 </Nav.Link>
+            }
+          </div>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
 
 export default SoomtutNavbar;

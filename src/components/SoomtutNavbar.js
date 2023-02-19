56efb51497a2212f4c6996e78a6b01f365569365
lalyns <kelwho@naver.com>
2023-02-19 22:57:19 +0900

    테스트 코드 삭제

--- src/components/SoomtutNavbar.js
+++ src/components/SoomtutNavbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 
 import styles from '../assets/styles/navstyles.module.css'

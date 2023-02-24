b426f0a30681f16070b61f622bca3cddb52ad63a
JungSungYun <kelwho@naver.com>
2023-02-24 17:33:37 +0900

    #10 [Feat] 지도 구현

--- src/routes/findtutor/FindTutor.js
+++ src/routes/findtutor/FindTutor.js
@@ -5,18 +5,19 @@ import SoomtutNavbar from "../../components/SoomtutNavbar";
 
 function FindTutor() {
     const [View, token, member] = SoomtutNavbar();
-    
 
     const KakaoMap = () => {
     
         useEffect(()=>{
-          var container = document.getElementById('map');
-          var options = {
-            center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
-            level: 3
-          };
-          var map = new kakao.maps.Map(container, options);
-          }, [])
+            if(member) {
+                var container = document.getElementById('map');
+                var options = {
+                    center: new kakao.maps.LatLng(member.vectorX, member.vectorY),
+                    level: 3
+                };
+                var map = new kakao.maps.Map(container, options);
+            }
+          }, [member])
       
           return (
               <div style={{ display:'flex' }}>

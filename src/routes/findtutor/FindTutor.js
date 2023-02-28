5972402cc439553cc632dd833f8b9219e48705b0
JungSungYun <kelwho@naver.com>
2023-02-28 10:55:27 +0900

    # [Feat] 마커 이벤트 추가

--- src/routes/findtutor/FindTutor.js
+++ src/routes/findtutor/FindTutor.js
@@ -5,45 +5,49 @@ import axios from "axios"
 import CustomNavbar from "../../components/CustomNavbar";
 import FindTutorM from "../../assets/styles/findTutor.css"
 
-function overLayMake(nearLocations,map,markTmp, isOpen){
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
-   
+// function overLayMake(nearLocations, map, markTmp, isOpen){
 
-    return function(){
-       
-        var content = `<div class="wrap"> 
-            <div class="info"> 
-            <div class="title">  
-                ${nearLocations.nickname}
-            </div>  
-            <div class="body"> 
-                <div class="img">
-                    <img src="${nearLocations.image}" width="73" height="70"/>
-            </div>
-                <div class="desc">
-                    <div class="ellipsis">${nearLocations.address}</div>
-                    <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>
-                </div>
-            </div>
-        </div> 
-    </div>`
- // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+//     var ret = false;
+
+//     const visibleHandler = (event) => {
+//         console.log(event.target);
+//     }
+
+//     const Marker = () => {
+//         var content =(
+//             `<div className="wrap" onClick={(event) => visibleHandler(event)}> 
+//                 <div className="info"> 
+//                     <div className="title">  
+//                         ${nearLocations.nickname}
+//                     </div>  
+//                     <div className="body"> 
+//                         <div className="img">
+//                             <img src="${nearLocations.image}" style={{width:"73px", height:"70px"}}/>
+//                         </div>
+//                         <div className="desc">
+//                             <div className="ellipsis">${nearLocations.address}</div>
+//                             <div><a href="https://www.kakaocorp.com/main" target="_blank" className="link">홈페이지</a></div>
+//                         </div>
+//                     </div>
+//                 </div> 
+//             </div>`)
+//         ;
+//  // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
 
 
-        var overlay = new kakao.maps.CustomOverlay({
-        content : content,
-        position: markTmp.getPosition(),
-        });
+//         var overlay = new kakao.maps.CustomOverlay({
+//         content : content,
+//         position: markTmp.getPosition(),
+//         });
+//         if (!isOpen) {overlay.setMap(map); ret = true;} 
+//         else {overlay.setMap(null); ret = false;}
+//     }       
 
-    if (!isOpen) {
-        overlay.setMap(map);
-        
-    } else {
-        overlay.setMap(null);
-    }
+//     return [Marker, ret]
+// }
 
-}
-    }
 
     
 
@@ -51,116 +55,116 @@ function FindTutor() {
 
     const [View, token, member] = CustomNavbar();
 
+    const [loading, setLoading] = useState(false);
+
     const [isOpen, setIsOpen] = useState(false);
+    const [otherMarkers, setOtherMarkers] = useState(null);
 
     const KakaoMap = () => {
-    
-        useEffect(()=>{
+
+        useEffect(() => {
+            if(!loading) GetMarkerInfo()
+        }, [member, token] )
+
+        const GetMarkerInfo = () => {
+            var config = {
+                method: 'get',
+                maxBodyLength: Infinity,
+                url: `http://localhost:8080/location/showNearTutor`,
+                headers: { 
+                'Authorization': token,
+                'Content-Type':'application/json'
+                },
+            };
             
+            axios(config)
+            .then(function (response) {
+                console.log(response.data)
+                setOtherMarkers(response.data)
+                setLoading(true);
 
-            if(member) {
+            })
+            .catch(function (error) {
+                console.log(error)
+            });
+        }
 
-                var container = document.getElementById('map');
-                var options = {
-                    center: new kakao.maps.LatLng(member.vectorX, member.vectorY),
-                    level: 5
-                };
-                var map = new kakao.maps.Map(container, options);
-
-                var config = {
-                    method: 'get',
-                    maxBodyLength: Infinity,
-                    url: 'http://localhost:8080/location/showNearTutor',
-                    headers: { 
-                      'Authorization': token,
-                      'Content-Type':'application/json'
-                    },
-                  };
-                  
-                  axios(config)
-                  .then(function (response) {
-                    
-                    var nearLocations = response.data;
-                    var radius = 5000;
-                    var markTmp;
-                    var overlay;
-
-
-
-                    var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다    
-                                    imageSize = new kakao.maps.Size(40, 50), // 마커이미지의 크기입니다
-                                    imageOption = {offset: new kakao.maps.Point(27, 69)};
-
-                     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-                    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-                        markerPosition = new kakao.maps.LatLng(member.vectorX, member.vectorY); // 마커가 표시될 위치입니다
-
-
-                       var mymarker = new kakao.maps.Marker({
-                                position: markerPosition,
-                                clickable: true,
-                                image : markerImage   
-                            });
-                            mymarker.setMap(map);
-
-                    for (var i=0;i<nearLocations.length;i++) {
-
-                        markTmp = new kakao.maps.Marker({
-                        position: new kakao.maps.LatLng(nearLocations[i].vectorX, nearLocations[i].vectorY),
-                        title: nearLocations[i].nickname,
-                        map: map
-                    });
-
-               
-
-                
-                    var v = new kakao.maps.Polyline({
-                        path: [markerPosition, new kakao.maps.LatLng(nearLocations[i].vectorX, nearLocations[i].vectorY)]
-                    });
-
-                
-                    if(v.getLength()<=radius){
-    
-                            markTmp.setMap(map);
-                            
-                            kakao.maps.event.addListener(markTmp, 'click', overLayMake(nearLocations[i],map,markTmp, isOpen));
-                            
-                            setIsOpen(!isOpen);
-
-                                  
-                    }
-                    
-
-                }
-                
-
-                  })
-                  .catch(function (error) {
-                    console.log(error);
-                  }); 
-                  
+        const MakeOtherMarkers = () => {
+            if(otherMarkers) {
+                return (
+                    otherMarkers.map((item, index) => item.nickname === member.nickname ? 
+                        null :
+                        <MapMarker
+                         key={index}
+                         position= {{
+                            lat: item.vectorX,
+                            lng: item.vectorY
+                         }}
+                         clickable={true}
+                         onClick={() => setIsOpen(true)}
+                         >
+                            {isOpen && <div style={{ padding: "5px", color: "#000" }}>
+                            Hello World! <br />
+                            <a
+                                href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667"
+                                style={{ color: "blue" }}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                큰지도보기
+                            </a>{" "}
+                            <a
+                                href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667"
+                                style={{ color: "blue" }}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                길찾기
+                            </a>
+                            </div>}
+                        </MapMarker>)
+                );
             }
-          }, [member])
-      
-          return (
-              <div style={{ display:'flex' }}>
-                  <div id='map'  style={{
-                          width:'100%',
-                          height:'100vh',
-                  }}> </div>
-                  
-                  {/* <div  id=“tutorList”style={{
-                      minWidth: ‘330px’,
-                      height:‘85vh’,
-                      backgroundColor:“white”
-                      
-                  }}> */}
+        }
+
+        useEffect(() => {
+            if(otherMarkers) { MakeOtherMarkers() }
+        }, [otherMarkers])
+        
+
+        if(member) {
+            return (
+                <Map center={{
+                    lat: member.vectorX,
+                    lng: member.vectorY
+                }} style= {{
+                    width: "100%",
+                    height: "100vh"
+                }}>
+                  <MapMarker key="myMarker"
+                   position={{ lat: member.vectorX, lng: member.vectorY }}
+                   image={{
+                    src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png", // 마커이미지의 주소입니다
+                    size: {
+                      width: 64,
+                      height: 69,
+                    }, // 마커이미지의 크기입니다
+                    options: {
+                      offset: {
+                        x: 27,
+                        y: 69,
+                      }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+                    },
+                  }}
+                  > 
+                   </MapMarker>
+                   <MakeOtherMarkers />
+
                     
-      
-                  {/* </div> */}
-              </div>
-          );
-      }
+                </Map>
+            )
+        }
+    }
 
       return (
         <div>
@@ -169,5 +173,4 @@ function FindTutor() {
         </div>
       )
 }
-
 export default FindTutor

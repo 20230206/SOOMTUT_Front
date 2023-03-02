1a9ddc1e38e1fe5368dd6092fa328cc85d51a1d8
JungSungYun <kelwho@naver.com>
2023-03-03 01:52:13 +0900

    [feat] env 값 추가

--- src/routes/lecture/GetLecture.js
+++ src/routes/lecture/GetLecture.js
@@ -123,7 +123,7 @@ function GetLecture() {
         const windowLeft = window.screenLeft + window.innerWidth / 2 - windowWidth / 2;
         const windowTop = window.screenTop + window.innerHeight / 2 - windowHeight / 2;
         const windowFeatures = `width=${windowWidth},height=${windowHeight},left=${windowLeft},top=${windowTop}`;
-        window.open(`http://localhost:3000/chat/${lectureId}`, "_blank", windowFeatures);
+        window.open(`${process.env.REACT_APP_FRONT}/chat/${lectureId}`, "_blank", windowFeatures);
 
     }
 
